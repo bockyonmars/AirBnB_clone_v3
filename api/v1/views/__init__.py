17cea73e37adfd72ff93fa11a6c8d1131cb8ea71
bockyonmars <onyema.bosworth@gmail.com>
2025-07-30 09:33:50 +0100

    l

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""Initialize the views blueprint"""
+"""Initialize Blueprint and register all routes"""
 
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+# Create a single blueprint instance
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# Import views to register routes with the blueprint
+# Import views to register routes with this blueprint
 from api.v1.views.index import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
 from api.v1.views.places import *
-from api.v1.views.reviews import *
+from api.v1.views.places_amenities import *
+from api.v1.views.places_reviews import *
 from api.v1.views.states import *
 from api.v1.views.users import *
