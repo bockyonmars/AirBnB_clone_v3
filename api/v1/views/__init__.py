e8728ad868d6e194f71b7c8283e88605b2547630
bockyonmars <onyema.bosworth@gmail.com>
2025-07-30 06:34:28 +0100

    y

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Initialize the views blueprint"""
+
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+# Import views to register routes with the blueprint
+from api.v1.views.index import *
+from api.v1.views.amenities import *
+from api.v1.views.cities import *
+from api.v1.views.places import *
+from api.v1.views.reviews import *
+from api.v1.views.states import *
+from api.v1.views.users import *
